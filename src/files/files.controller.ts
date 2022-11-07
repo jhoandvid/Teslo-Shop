1607208f83fa2b4cd0286daf386a64bf643f1ee2
jhoandvid <rojasalmariojhoandvid@gmail.com>
2022-11-07 11:12:59 -0500

    rename uploaded files

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, Upl
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { FilesService } from './files.service';
-import { fileFilter } from './helpers/fileFilter.helper';
+import { fileFilter, fileNamer } from './helpers';
 
 @Controller('files')
 export class FilesController {
@@ -13,7 +13,8 @@ export class FilesController {
     fileFilter:fileFilter,
     //limits:{fileSize:1000}
     storage: diskStorage({
-      destination: './static/uploads',
+      destination: './static/products',
+      filename:fileNamer
       
     })
   }))
